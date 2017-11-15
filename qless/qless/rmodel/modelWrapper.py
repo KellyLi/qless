d2e206e33d163da643750b38be271bdb183eede7
Evan Yeung <evan.yeung1@gmail.com>
2017-11-15 13:34:27 -0500

    Added function to call in to R model

---
+++ qless/qless/rmodel/modelWrapper.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+
+dir = os.path.dirname(__file__)
+
+COMMAND = 'Rscript'
+#PATH = os.path.join(dir, 'predictFromModel.R')
+PATH = 'predictFromModel.R'
+
+def estimateWaitTime(
+		arrival_time, # number (minutes since 00:00)
+		weekday, # string ('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun')
+		flow_rate, # number (people seen in last hour)
+		queue_length, # number
+		doctor, # string ('a', 'b', 'c', etc.)
+		appointment_time, # number (minutes since 00:00)
+		isWalkIn, # boolean
+	):
+	args = [str(arrival_time), str(weekday), str(flow_rate), str(queue_length),
+			str(doctor), str(appointment_time), str(int(isWalkIn))]
+	cmd = [COMMAND, PATH] + args
+	result = subprocess.check_output(cmd, universal_newlines=True, cwd=dir)
+	return float(result.split(' ')[-1].strip())
+
