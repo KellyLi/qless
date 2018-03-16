8b4fc4dc17083c26047202c367e53703f02c5905
Evan Yeung <evan.yeung1@gmail.com>
2018-03-16 16:33:34 -0400

    Add SQL create command

---
+++ qless/qless/database/create_command.sql
@@ -0,0 +1,15 @@
+CREATE TABLE qless (
+  arrival_time INT,
+  weekday VARCHAR(3),
+  flow_rate INT,
+  queue_length INT,
+  doctor INT,
+  appt_time INT,
+  is_walk_in BOOLEAN,
+  num_doctors INT,
+  month INT,
+  seen_time INT,
+  estimated_wait_time INT,
+  timestamp DATETIME,
+  has_been_fitted BOOLEAN
+);
